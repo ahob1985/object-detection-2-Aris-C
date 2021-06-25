96e7ff3e993b1808c8a877a84b9c479ff924f0f1
ArisC1 <67166970+Aris-C@users.noreply.github.com>
2021-06-25 20:47:36 +0000

    Finished project

--- script.js
+++ script.js
@@ -13,35 +13,37 @@ let detections;
 let isModelReady;
 
 function setup() {
-canvasDiv(); = createDiv();
-canvas = createCanvas(640, 480);
-canvas.parent(canvasDiv);
-textDiv  = createDiv();
-textP = ("Model is loading please wait...");
-textP.parent(textDiv);
-//load Video
-video = createCapture(VIDEO, videoReady);
-detections = [];
-isModelReady = false;
-
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv  = createDiv();
+  textP = createP("Model is loading please wait...");
+  textP.parent(textDiv);
+  // //load Video
+  video = createCapture(VIDEO, videoReady);
+  detections = [];
+  isModelReady = false;
 }
 
 function draw() {
  
  if(isModelReady){ 
-    image(video, 0, 0);
-   detector.detect(canvas, gotResults);
+  image(video, 0, 0);
+  detector.detect(canvas, gotResults);
+  for (let i = 0; i < detections.length; i++){
+    drawLabel(detections[i]);
+
+  }
  }
 }
 
 function videoReady() {
-  video.style = ("display", "none");
-  dectector = ml5.objectDetector("cocossd", modelReady);
+  video.style("display", "none");
+  detector = ml5.objectDetector("cocossd", modelReady);
 }
 
 function modelReady() {
-  isModelReady = true
+  isModelReady = true;
 }
 
 function drawLabel(object) {
@@ -51,17 +53,19 @@ function drawLabel(object) {
   rect(object.x, object.y, object.width, object.height);
 
   // Draw the label and its confidence value near the object
+  noStroke();
   fill(0, 0,200);
   textSize(21);
-  let label = object.label
-  let confidence = floor(object.confidence*100);
-  text(label + ": " + confidence + " %" + object.x + 5, object.y + 20);
+  let label = object.label;
+  let confidence = floor(object.confidence * 100);
+  text(label + ": " + confidence + " %", object.x + 5, object.y + 20);
+}
 
 function gotResults(error, results) {
   if(error){
     console.error(error);
-
   } else {
-    textP.html
+    textP.html("I detect these objects!");
+    detections = results;
   }
 }
